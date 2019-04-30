56a949c69511cb8ba69acac647551242ec8713e1
elepearson <48634483+elepearson@users.noreply.github.com>
2019-04-30 15:38:00 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body style="background-color: #93eaf2;">
+
+<picture>
+  <source srcset="1.png" media="(max-width: 600px)">
+  <source srcset="img_flowers.jpg" media="(max-width: 1500px)">
+  <source srcset="flowers.jpg">
+  <img src="img_flowers.jpg" alt="Flowers" style="width:auto;">
+</picture>
+
+</body>
+</html>
